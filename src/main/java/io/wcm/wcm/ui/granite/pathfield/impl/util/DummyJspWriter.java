5f6ddeda3d646f9c647d7a9c4e69ecfde1c80772
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-02-12 10:57:57 +0100

    DummyJspWriter: Ignore but accept all write methods to avoid conflicts with intrusive filters.

--- src/main/java/io/wcm/wcm/ui/granite/pathfield/impl/util/DummyJspWriter.java
+++ src/main/java/io/wcm/wcm/ui/granite/pathfield/impl/util/DummyJspWriter.java
@@ -33,136 +33,137 @@ class DummyJspWriter extends JspWriter {
     super(0, false);
   }
 
-
-  // --- unsupported methods ---
+  // --- ignore but accept all writing methods 
 
   @Override
   public void newLine() throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(boolean b) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(char c) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(int i) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(long l) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(float f) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(double d) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(char[] s) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(String s) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void print(Object obj) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println() throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(boolean x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(char x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(int x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(long x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(float x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(double x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(char[] x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(String x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void println(Object x) throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void clear() throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void clearBuffer() throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void flush() throws IOException {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void close() throws IOException {
-    throw new UnsupportedOperationException();
-  }
-
-  @Override
-  public int getRemaining() {
-    throw new UnsupportedOperationException();
+    // ignore
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
+    // ignore
+  }
+
+  // --- unsupported methods ---
+
+  @Override
+  public int getRemaining() {
     throw new UnsupportedOperationException();
   }
 
