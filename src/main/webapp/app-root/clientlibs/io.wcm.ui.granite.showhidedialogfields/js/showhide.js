e82070abb6bd9f7f1194e06c6bfeca6791450349
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-08 11:08:31 +0200

    fix indentation

--- src/main/webapp/app-root/clientlibs/io.wcm.ui.granite.showhidedialogfields/js/showhide.js
+++ src/main/webapp/app-root/clientlibs/io.wcm.ui.granite.showhidedialogfields/js/showhide.js
@@ -157,33 +157,33 @@
   }
 
   function toggleHiddenInput($element, show) {
-      if (show) {
-          $element.find("input:hidden[data-was-hidden]")
-              .each(function (index, field) {
-                  var $field = $(field);
-                  $field.removeAttr("data-was-hidden");
-                  $field.removeAttr("disabled");
-              });
-          if ($element.attr("type") === "hidden" && $element.attr("data-was-hidden") !== undefined) {
-              $element.removeAttr("data-was-hidden");
-              $element.removeAttr("disabled");
-          }
-      } else {
-          $element.find("input:hidden")
-              .filter(":not([data-was-hidden])")
-              .filter(":not([disabled])")
-              .filter(":not([name$='@Delete'])")
-              .each(function (index, field) {
-                  var $field = $(field);
-                  $field.attr("data-was-hidden", true);
-                  $field.attr("disabled", true);
-              });
-          if ($element.attr("type") === "hidden" && $element.attr("disabled") === undefined &&
-              $element.attr("name") !== undefined && !$element.attr("name").endsWith("@Delete")) {
-              $element.attr("data-was-hidden", true);
-              $element.attr("disabled", true);
-          }
+    if (show) {
+      $element.find("input:hidden[data-was-hidden]")
+        .each(function (index, field) {
+          var $field = $(field);
+          $field.removeAttr("data-was-hidden");
+          $field.removeAttr("disabled");
+        });
+      if ($element.attr("type") === "hidden" && $element.attr("data-was-hidden") !== undefined) {
+        $element.removeAttr("data-was-hidden");
+        $element.removeAttr("disabled");
       }
+    } else {
+      $element.find("input:hidden")
+        .filter(":not([data-was-hidden])")
+        .filter(":not([disabled])")
+        .filter(":not([name$='@Delete'])")
+        .each(function (index, field) {
+          var $field = $(field);
+          $field.attr("data-was-hidden", true);
+          $field.attr("disabled", true);
+        });
+      if ($element.attr("type") === "hidden" && $element.attr("disabled") === undefined &&
+        $element.attr("name") !== undefined && !$element.attr("name").endsWith("@Delete")) {
+        $element.attr("data-was-hidden", true);
+        $element.attr("disabled", true);
+      }
+    }
   }
 
   /**
